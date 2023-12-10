979f6aa804ee4fae97278363cd23168eaaa29e0f
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-12-10 22:01:04 +0100

    0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 #sets up your web servers for the deployment of web_static
 sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
 sudo bash -c 'cat  >/data/web_static/releases/test/index.html <<EOF
 <html>
   <head>
@@ -16,14 +17,16 @@ if [ -L "$link_path" ]; then
     sudo rm "$link_path"
 fi
 sudo ln -s "$target" "$link_path"
-sudo chown -R ubuntu:ubuntu /data/
+sudo chown -R ubuntu:ubuntu /data/*
 
 site_config="/etc/nginx/sites-enabled/mysite.conf"
 web_static_path="/data/web_static/current/"
 alias_path="/hbnb_static"
 domain="souf.tech"
 # Check if the configuration block already exists
-touch "$site_config"
+if ! [ -f "$site_config" ]; then 
+   sudo touch "$site_config"
+fi
 if ! grep -q "location $alias_path {" "$site_config"; then
     # Append the configuration block
     echo "server { 
