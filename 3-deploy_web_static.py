770556a9c7bc61d1070caf0e11e5ec8ad93b166a
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-12-24 22:35:53 +0100

    3-deploy_web_static.py

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -4,9 +4,17 @@ that distributes an archive to your web servers, using the
 function do_deploy:"""
 from fabric.api import run, env, put, local
 import os
-
 """ Fabric script (based on"""
 env.hosts = ["54.152.81.82", "3.94.185.211"]
+
+
+def do_pack():
+    """A function that generates a .tgz archive"""
+    local("if ! [ -d versions ]; then mkdir versions; fi")
+    output = local("date +%Y%m%d%M%S", capture=True)
+    value = local(f'tar -cvzf ./versions/web_static_{output}.tgz\
+ ./web_static', capture=True)
+    return f"./versions/web_static_{output}.tgz"
 
 
 def do_deploy(archive_path):
@@ -35,9 +43,21 @@ def do_deploy(archive_path):
     if run("rm -rf /data/web_static/releases/{}/web_static".
            format(name)).failed is True:
         return False
-    if run("rm -rf /data/web_static/current/").failed is True:
+    if run("rm -rf /data/web_static/current").failed is True:
         return False
     if run("ln -s /data/web_static/releases/{} /data/web_static/current".
            format(name)).failed is True:
         return False
+    if run("sudo service nginx restart").failed:
+        return False
     return True
+
+
+def deploy():
+    """creates and distributes an archive
+    to your web servers, using the function deploy:"""
+
+    path = do_pack()
+    print(path)
+    status = do_deploy(path)
+    return status
