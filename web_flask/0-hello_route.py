91bb467cb19fc6607a5714156b89879057e8a6fe
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-12-26 22:45:29 +0100

    task 1-->2

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -13,10 +13,6 @@ def hello():
     """display “Hello HBNB!”"""
     return ("Hello HBNB!")
 
-@app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    return ("HBNB")
-
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
